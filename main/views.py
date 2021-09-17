491a305f2e14552e2c41c46b16a3157442fc1456
Jameel Kaisar <jameelkaisarkhan@gmail.com>
2021-09-17 12:57:01 +0530

    User Management System Added

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Book
 # from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
@@ -35,3 +35,30 @@ def register(request):
         request=request,
         template_name="main/register.html",
         context={"form": form})
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password  = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}!")
+                return redirect("main:homepage")
+            else:
+                messages.error(request, "Invalid username or password!")
+        else:
+            messages.error(request, "Invalid username or password!")
+
+    form = AuthenticationForm()
+    return render(
+        request=request,
+        template_name="main/login.html",
+        context={"form": form})
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("main:homepage")
