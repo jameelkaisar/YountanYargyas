6da8c4efd40baab99e2671b305acaa47b5cd4443
Jameel Kaisar <jameelkaisarkhan@gmail.com>
2021-09-23 20:37:29 +0530

    Frontend Designed

--- YountanYargyas/settings.py
+++ YountanYargyas/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "192.168.91.239"]
+ALLOWED_HOSTS = ["127.0.0.1", "192.168.30.239"]
 
 
 # Application definition
--- main/forms.py
+++ main/forms.py
@@ -5,26 +5,27 @@ class AddClass(forms.ModelForm):
     class Meta:
         model = StudentClass
         fields = ["student_class", "class_summary"]
-        labels = {"student_class": "Student Class", "class_summary": "Class Summary"}
-        widgets = {"class_summary": forms.Textarea}
+        labels = {"student_class": "Class Name", "class_summary": "Class Summary"}
+        widgets = {"class_summary": forms.Textarea(attrs={"class": "materialize-textarea"})}
+
 
 class AddSubject(forms.ModelForm):
     class Meta:
         model = StudentSubject
         fields = ["student_subject", "subject_summary", "student_class"]
-        labels = {"student_subject": "Student Subject", "subject_summary": "Subject Summary", "student_class": "Student Class (FK)"}
-        widgets = {"subject_summary": forms.Textarea, "student_class": forms.HiddenInput}
+        labels = {"student_subject": "Subject Name", "subject_summary": "Subject Summary", "student_class": "Student Class (FK)"}
+        widgets = {"subject_summary": forms.Textarea(attrs={"class": "materialize-textarea"}), "student_class": forms.HiddenInput()}
 
 class AddChapter(forms.ModelForm):
     class Meta:
         model = StudentChapter
         fields = ["student_chapter", "chapter_summary", "student_subject"]
-        labels = {"student_chapter": "Student Chapter", "chapter_summary": "Chapter Summary", "student_subject": "Student Subject (FK)"}
-        widgets = {"chapter_summary": forms.Textarea, "student_subject": forms.HiddenInput}
+        labels = {"student_chapter": "Chapter Name", "chapter_summary": "Chapter Summary", "student_subject": "Student Subject (FK)"}
+        widgets = {"chapter_summary": forms.Textarea(attrs={"class": "materialize-textarea"}), "student_subject": forms.HiddenInput()}
 
 class AddSection(forms.ModelForm):
     class Meta:
         model = StudentSection
         fields = ["student_section", "section_summary", "section_video", "section_text", "student_chapter"]
-        labels = {"student_section": "Student Section", "section_summary": "Section Summary", "section_video": "Section Video (Optional)" , "section_text": "Section Text", "student_chapter": "Student Chapter (FK)"}
-        widgets = {"section_summary": forms.Textarea, "student_chapter": forms.HiddenInput}
+        labels = {"student_section": "Section Name", "section_summary": "Section Summary", "section_video": "", "section_text": "Section Text", "student_chapter": "Student Chapter (FK)"}
+        widgets = {"section_summary": forms.Textarea(attrs={"class": "materialize-textarea"}), "section_video": forms.FileInput(attrs={"style": "display: none;"}), "section_text": forms.Textarea(attrs={"class": "materialize-textarea"}), "student_chapter": forms.HiddenInput()}
