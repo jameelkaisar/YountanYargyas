aa0f99c19695a6430f02c2f1b9cfbb9760f8f27d
Jameel Kaisar <jameelkaisarkhan@gmail.com>
2021-11-18 23:26:43 +0530

    STATIC_ROOT Added

--- YountanYargyas/settings.py
+++ YountanYargyas/settings.py
@@ -165,6 +165,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
 # Media files
