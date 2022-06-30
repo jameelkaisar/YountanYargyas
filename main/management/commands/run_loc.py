bc1d04914fe4d332026260dba66c94230a9667c2
Jameel Kaisar <65322473+JameelKaisar@users.noreply.github.com>
2022-06-30 16:15:14 +0530

    Update run_loc.py

--- main/management/commands/run_loc.py
+++ main/management/commands/run_loc.py
@@ -6,11 +6,15 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--port', default=8000)
+        parser.add_argument('--insecure', default=None, action='store_true')
 
     def handle(self, *args, **options):
         try:
             self.stdout.write(f"Server Address: 127.0.0.1:{options['port']}")
-            call_command("runserver", f"127.0.0.1:{options['port']}")
+            if options['insecure'] is None:
+                call_command("runserver", f"0.0.0.0:{options['port']}")
+            else:
+                call_command("runserver", f"0.0.0.0:{options['port']}", "--insecure")
         except Exception as err:
             self.stdout.write("")
             if hasattr(err, '__iter__'):
